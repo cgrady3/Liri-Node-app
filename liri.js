53ef997b87c9d999efb913369ba023cf2fdcef2a
Carolyn Grady <owenv8@gmail.com>
2020-02-21 11:52:08 -0500

    spotify api function added, reads from random.txt to search spotify

--- liri.js
+++ liri.js
@@ -5,20 +5,69 @@ const keys = require("./keys.js");
 const Spotify = require('node-spotify-api');
 const inquirer = require('inquirer');
 
+function readingFile() {
+    // Reads the random text file and passes it to the spotify function
+    
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        
+        getSpotify(data);
+    });
+}
+
 //spotify
-var spotify = new Spotify(keys.spotify);
+// use inquire to get songName
+function getSpotify(songName) {
+    let spotify = new Spotify(keys.spotify);
+    
+    spotify.search({
+        type: 'track',
+        query: 'All the Small Things',
+        limit: 20
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+    
+        console.log(data);
+    });
+}
 
-spotify.search({
-    type: 'track',
-    query: 'All the Small Things',
-    limit: 20
-}, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
 
-    console.log(data);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // axios
 // Make a request for a user with a given ID
