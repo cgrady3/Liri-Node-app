f2894b7a46d71a90d8fa737fbb04beb29bcbf823
Carolyn Grady <owenv8@gmail.com>
2020-02-21 12:30:17 -0500

    spotify api search and return from txt file working

--- liri.js
+++ liri.js
@@ -1,18 +1,18 @@
-const dotenv = require("dotenv").config();
+const dotenv = require('dotenv').config();
 const axios = require('axios').default;
 const fs = require('fs');
-const keys = require("./keys.js");
+const keys = require('./keys.js');
 const Spotify = require('node-spotify-api');
 const inquirer = require('inquirer');
 
 function readingFile() {
     // Reads the random text file and passes it to the spotify function
-    
-    fs.readFile("random.txt", "utf8", function(err, data) {
+
+    fs.readFile('random.txt', 'utf8', function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        
+        console.log(data)
         getSpotify(data);
     });
 }
@@ -20,22 +20,38 @@ function readingFile() {
 //spotify
 // use inquire to get songName
 function getSpotify(songName) {
+
     let spotify = new Spotify(keys.spotify);
-    
+
     spotify.search({
         type: 'track',
-        query: 'All the Small Things',
-        limit: 20
+        query: songName,
+        limit: 10,
     }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-    
-        console.log(data);
+        // console.log(data.tracks.items[1])
+        var songs = data.tracks.items;
+
+        console.log('----------queried song info----------');
+
+        for (var i = 0; i < songs.length; i++) {
+            console.log('--------------------------');
+            console.log('Song #' + (i+1));
+            console.log('Song name: ' + songs[i].name);
+            console.log('Album: ' + songs[i].album.name);
+            console.log('Artist(s): ' + songs[i].artists[0].name);
+            console.log('Listen Here: ' + songs[i].album.external_urls.spotify);
+            console.log('----------------------------');  
+
+         }
+
+        // console.log(data);
     });
 }
 
-
+readingFile()
 
 
 
@@ -71,34 +87,34 @@ function getSpotify(songName) {
 
 // axios
 // Make a request for a user with a given ID
-axios.get('/user?ID=')
-    .then(function (response) {
-        // handle success
-        console.log(response);
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .finally(function () {
-        // always executed
-    });
+// axios.get('/user?ID=')
+//     .then(function (response) {
+//         // handle success
+//         console.log(response);
+//     })
+//     .catch(function (error) {
+//         // handle error
+//         console.log(error);
+//     })
+//     .finally(function () {
+//         // always executed
+//     });
 
-// GET request for remote image
-axios({
-    method: 'get',
-    url: 'http://bit.ly/2mTM3nY',
-    responseType: 'stream'
-})
-    .then(function (response) {
-        response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
-    });
+// // GET request for remote image
+// axios({
+//     method: 'get',
+//     url: 'http://bit.ly/2mTM3nY',
+//     responseType: 'stream'
+// })
+//     .then(function (response) {
+//         response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+//     });
 
-// OMDb API
-// Send all data requests to:
+// // OMDb API
+// // Send all data requests to:
 
-// http://www.omdbapi.com/?apikey=[]&
+// // http://www.omdbapi.com/?apikey=[]&
 
-// Poster API requests:
+// // Poster API requests:
 
-// http://img.omdbapi.com/?apikey=[]&
+// // http://img.omdbapi.com/?apikey=[]&
